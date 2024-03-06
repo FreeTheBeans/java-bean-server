727b1284a08e75c20804d63b9e9067768542156e
tristanphips <tristan.phipson@bbd.co.za>
2024-03-06 19:12:30 +0200

    dev: added SQL (liquibase) for basic DDL.

--- sql/DDL/players.sql
+++ sql/DDL/players.sql
@@ -4,6 +4,6 @@
 CREATE TABLE "players" (
   "player_id" SERIAL NOT NULL PRIMARY KEY,
   "username" varchar NOT NULL,
-  "last_game_state" varchar DEFAULT "start_state",
+  "last_game_state" varchar DEFAULT 'start_state',
   "uid" varchar
 );
