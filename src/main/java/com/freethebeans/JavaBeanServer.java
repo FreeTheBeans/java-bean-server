9044cb8b2573221be1a067e955b033d9551cb254
tristanphips <tristan.phipson@bbd.co.za>
2024-03-09 20:02:48 +0200

    feature/db-connection: added DB connection info, through SecretsManager.

--- src/main/java/com/freethebeans/JavaBeanServer.java
+++ src/main/java/com/freethebeans/JavaBeanServer.java
@@ -3,10 +3,22 @@ package com.freethebeans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import com.freethebeans.server.controller.DBConnection;
+
 @SpringBootApplication
 public class JavaBeanServer {
+    // final DBConnection dbConnection = new DBConnection();
 
     public static void main(String[] args) {
+        // dbConnection.getDummyRecord();
         SpringApplication.run(JavaBeanServer.class, args);
+        printStartBanner();
+    }
+
+    private static void printStartBanner() {
+        System.out.println("===============================================");
+        System.out.println("|        The server has started successfully!   |");
+        System.out.println("|            Let the journey begin!             |");
+        System.out.println("===============================================");
     }
 }
