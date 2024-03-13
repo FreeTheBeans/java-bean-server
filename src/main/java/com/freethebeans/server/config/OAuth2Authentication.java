eeec0bb42c80129f2f55902dfd251d938c6408a9
FaheemahPatel <faheemah@bbd.co.za>
2024-03-13 20:30:10 +0200

    fixed code

--- src/main/java/com/freethebeans/server/config/OAuth2Authentication.java
+++ src/main/java/com/freethebeans/server/config/OAuth2Authentication.java
@@ -2,25 +2,19 @@ package com.freethebeans.server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
 @Component
 public class OAuth2Authentication {
 
-    private static final long TIMEOUT_DURATION_MS = 60000;
-
-    @Autowired
-    private OAuth2Config oAuth2Config;
-
-    private Scanner scanner;
+    private final OAuth2Config oAuth2Config;
+    private final Scanner scanner;
     public String logoutLink = "http://localhost:31415/user-logout";
 
-    public OAuth2Authentication() {
+    @Autowired
+    public OAuth2Authentication(OAuth2Config oAuth2Config) {
+        this.oAuth2Config = oAuth2Config;
         this.scanner = new Scanner(System.in);
     }
 
@@ -43,44 +37,10 @@ public class OAuth2Authentication {
     }
 
     private void performOAuth2Authentication(CountDownLatch latch) {
-//        if (GraphicsEnvironment.isHeadless()) {
-            System.out.println("Login via: " + oAuth2Config.getOauth2AuthorizationUrl());
-            latch.countDown();
-//        } else {
-//            openWebBrowserForLogin(latch);
-//        }
+        System.out.println("Login via: " + oAuth2Config.getOauth2AuthorizationUrl());
+        latch.countDown();
     }
-
-//    private void openWebBrowserForLogin(CountDownLatch latch) {
-//        try {
-//            Desktop desktop = Desktop.getDesktop();
-//
-//            if (desktop.isSupported(Desktop.Action.BROWSE)) {
-//                System.out.println("OAuth2 Authorization URL: " + oAuth2Config.getOauth2AuthorizationUrl());
-//
-//                URI uri = new URI(oAuth2Config.getOauth2AuthorizationUrl());
-//                desktop.browse(uri);
-//
-//                new Thread(() -> {
-//                    try {
-//                        Thread.sleep(TIMEOUT_DURATION_MS);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//
-//                    System.out.println("Closing browser...");
-//                    System.exit(0);
-//                }).start();
-//            } else {
-//                System.out.println("Unable to open browser. Manually open: " + oAuth2Config.getOauth2AuthorizationUrl());
-//                latch.countDown();
-//            }
-//        } catch (IOException | URISyntaxException e) {
-//            System.err.println("Error opening browser: " + e.getMessage());
-//            latch.countDown();
-//        }
-//    }
-    }
+}
 
 
 
