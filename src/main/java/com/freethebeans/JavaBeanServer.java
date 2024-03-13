a20639b35109d89d9f5dabf397a7edc2f31f5930
FaheemahPatel <faheemah@bbd.co.za>
2024-03-13 20:22:53 +0200

    changed autowired in main

--- src/main/java/com/freethebeans/JavaBeanServer.java
+++ src/main/java/com/freethebeans/JavaBeanServer.java
@@ -9,12 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaBeanServer implements CommandLineRunner {
 
-    @Autowired
-    private OAuth2Authentication oAuth2Authentication;
+    private final OAuth2Authentication oAuth2Authentication;
+
+    public JavaBeanServer(OAuth2Authentication oAuth2Authentication) {
+        this.oAuth2Authentication = oAuth2Authentication;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JavaBeanServer.class, args);
     }
+
     @Override
     public void run(String... args) throws Exception {
         oAuth2Authentication.performAuthentication();
